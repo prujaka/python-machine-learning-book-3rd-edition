5e9b5f5a5e70acc1ce7e3fa1dc352eaebea55da5
Sergey Tkachenko <sergeyprujina@gmail.com>
2023-01-19 11:29:37 +0100

    ch04

--- ch04/ch04.py
+++ ch04/ch04.py
@@ -82,7 +82,8 @@ from sklearn.feature_selection import SelectFromModel
 
 
 
-csv_data = '''A,B,C,D
+csv_data = \
+'''A,B,C,D
 1.0,2.0,3.0,4.0
 5.0,6.0,,8.0
 10.0,11.0,12.0,'''
@@ -376,7 +377,8 @@ df_wine.head()
 
 X, y = df_wine.iloc[:, 1:].values, df_wine.iloc[:, 0].values
 
-X_train, X_test, y_train, y_test =    train_test_split(X, y, 
+X_train, X_test, y_train, y_test =\
+    train_test_split(X, y, 
                      test_size=0.3, 
                      random_state=0, 
                      stratify=y)
@@ -538,7 +540,8 @@ class SBS():
 
     def fit(self, X, y):
         
-        X_train, X_test, y_train, y_test =             train_test_split(X, y, test_size=self.test_size,
+        X_train, X_test, y_train, y_test = \
+            train_test_split(X, y, test_size=self.test_size,
                              random_state=self.random_state)
 
         dim = X_train.shape[1]
